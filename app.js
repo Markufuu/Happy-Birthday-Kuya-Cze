8e39f7f03f3d108c93e296f79a6855d6e54ed5e9
Markufuu <madechavez22@gmail.com>
2022-06-14 23:56:34 +0800

    Added files

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const tl = gsap.timeline({defaults: {ease: "power1.out"} });
+const btn =  document.querySelector('.btn');
+const rickroll = document.querySelector('.rickroll');
 
 tl
     .to('.intro-img', {y: "0%" , duration: 2} )
-    .to('.intro-overlay', {y: "-100%", duration: 1.5})
-    .to('.intro', {y: "-100%", duration: 1}, "-=1")
+    .to('.intro-overlay', {y: "-100%", duration: 1})
+    .to('.intro', {y: "-100%", duration: 1}, "-=.8")
+
+
+
+
+
+
+btn.addEventListener('click', () => {
+    rickroll.style.zIndex = "9999";
+    rickroll.play();
+});
