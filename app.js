431d3d7c547c42abdc65d07ff597987c68792c23
Markufuu <madechavez22@gmail.com>
2022-06-14 17:45:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({defaults: {ease: "power1.out"} });
 
 tl
-    .to('.intro-img', {y: "0%" , duration: 1.5} )
+    .to('.intro-img', {y: "0%" , duration: 2} )
     .to('.intro-overlay', {y: "-100%", duration: 1.5})
     .to('.intro', {y: "-100%", duration: 1}, "-=1")
